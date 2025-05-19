e16dd8853f1e2ba870c51d2a1c05c125bfd8ca2f
Arthurobo <arthuron57@gmail.com>
2025-05-19 10:38:11 +0100

    Added Postgresql setup with compose.yml and corresponding settings update for database

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ docker run -p 8000:8000 blog
 <!-- this will listen for changes -->
 docker run -v $(pwd):/app -p 8000:8000 blog
     - NOTE: - -v $(pwd):/app → mounts your current host directory (project code) into the container at /app.
+
+docker compose up --build
+    - This will build and run with docker compose
+
+
+docker compose exec <service_name> python manage.py migrate 
+    - NOTE: To run django specific commands (NOTE: The django web container must be running.)
+    - in our own case, the command will be:
+    - docker compose exec blog python manage.py migrate
+    - docker compose exec blog python manage.py createsuperuser
--- blog/settings.py
+++ blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -72,12 +73,25 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.{}'.format(
+             os.getenv('DB_ENGINE', 'sqlite3')
+         ),
+         'NAME': os.getenv('DB_NAME'),
+         'USER': os.getenv('DB_USER'),
+         'PASSWORD': os.getenv('DB_PASSWORD'),
+         'HOST': os.getenv('DB_HOST'),
+         'PORT': os.getenv('DB_PORT'),
+     }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
